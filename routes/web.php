542459879ef9cab0cac9a17b8cd49fde149cc6af
youness <younessbakka7@gmail.com>
2023-03-19 15:24:37 +0100

    phase3  launch of the project

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::get('/', function () {
      return view('app');
     
 });
-Route::get('/users', function () {
+Route::get('/login', function () {
   return inertia::render('login');
     
 });
