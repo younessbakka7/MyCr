f9dd43a5233728b9c8d7222552d0e58d6593b22f
youness <younessbakka7@gmail.com>
2023-04-01 15:02:23 +0100

    interface home page

--- resources/js/pages/login.jsx
+++ resources/js/pages/login.jsx
@@ -1,37 +1,124 @@
 import React from 'react';
-import  './home.css';
-
+import { Link } from 'react-router-dom';
+import '../pages/home.css';
 const Login = () => {
   return (
-    <>
-    <h1>HELLO IN MyCr</h1>
-    <p><span>MyCr</span> with a goal to help reduce workplace accidents by providing high-quality products backed by expert advice and guidance.</p>
-      <h2>inscrite vous pour suivre notre service</h2>
+    <div>
+  <div> 
+  <form>
+  <input type="text" placeholder="Search..."/>
+  <button type="submit">Search</button>
+</form>
+  <ul>
+  <li ><a href="#home">Home</a></li>
+  <li><a href="#news">Voiture</a></li>
+  <li><a href="#Accesoire">Accesoire</a></li>
+  <li><a href="#admin">Admin</a></li>
+  <li className='tfo' ><a href="logout"  >logout</a></li>
+  </ul>
+  <div className='container'>
+  <img  src="ome-img.jpeg"  width="100%" height="100%" />
+  <h1 class="h1">MyCr</h1>
+  <div class="topleft">Decidedly Sporty Design, Accentuated By The Wheels As Standard. The Rear Has A Gritty Appearance Thanks To The Silver Skid Plate That Ensures Greater Safety In Off-Road Conditions.</div>
+  <a href='#' className='more'>autre service</a>
+  </div>
+  </div> 
 
-      <form className='form' action='' method='POST'>
-      <label for="">First Name</label><br /><br />
-      <input type="text" name="nom" placeholder="Nom" required/><br /><br />
-      <label for="">Last Namee</label><br /><br />
-      <input type="text" name="prenom" placeholder="Prenom" required/><br /><br />
-      <label for="">Email</label><br /><br />
-      <input type="text" name="Email" placeholder="Email" required/><br /><br />
-      
-      <button className="btn" name="sub">
-        valide
-      </button>
-    </form><br /><br /><br />
+  <div className='aboutus'>
+    <h1 className='us'>About Us</h1>
+    <img className='img' src="about.jpeg" alt="" />
+    <div className='text'>
+    <h3>What Makes Our Services Special?</h3>
+    <p>No Key Transfer With Our Technicians.
+Leave Your Car Unlocked Or Request For Your Technician To Contact You.</p><br /><br />
+
+    </div>
+    
+
+  </div >
+  <div className='row' >
+    <h1 className='POPULAR'>POPULAR BRANDS</h1>
+  <div className='bobila'>
+  <img src="ALpha.png" alt="" width="100%"/>
+  </div>
+  <div className='bobila'>
+  <img src="mercedes.png" alt="" width="100%" />
+  </div>
+  <div className='bobila'>
+  <img src="Suz.png" alt="" width="100%"  />
+  </div>
+
+  <div className='bobila'>
+  <img src="BMW.png" alt="" width="100%"  />
+  </div>
+  <div className='bobila'>
+  <img src="Wolswagen.png" alt="" width="100%"  />
+  </div>
+  <div className='bobila'>
+  <img src="SKODAA.png" alt="" width="100%"  />
+  </div>
 
 
-    <footer className="footer">
+
+
+
+  
+  
+  </div>
+    
+
+  <div class="grid-container">
+  <div>
+    <img src="BMW8.png" alt="" />
+    <h1 className='cahd'>BMW X4</h1>
+    <p className='cahd'>10.000 DH</p>
+  </div>
+  <div>
+    <img src="BMW8.png" alt="" />
+    <h1 className='cahd'>BMW X4</h1>
+    <p className='cahd'>10.000 DH</p>
+  </div>
+  <div>
+    <img src="BMW8.png" alt="" />
+    <h1 className='cahd'>BMW X4</h1>
+    <p className='cahd'>10.000 DH</p>
+  </div>
+  <div>
+    <img src="BMW8.png" alt="" />
+    <h1 className='cahd'>BMW X4</h1>
+    <p className='cahd'>10.000 DH</p>
+  </div>
+
+
+
+  </div>  
+
+  <footer className="footer">
       <p>MyCr</p>
       <p>younessbakka7@gmail.com</p>
-  
+    <div class="credit">created by <span>youness benbakka</span> | all rights reserved</div>
     </footer>
-    </>
 
-      
-      
-    
+
+
+
+  </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   );
 }
 
--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,5 @@ Route::get('/login', function () {
     
 });
 
+
+
